e92be2f6d5872a5dba5042d68543908ddc6266ef
Souluon <epson5503@gmail.com>
2021-03-15 21:03:04 +0600

    Register

--- src/authe/utils.py
+++ src/authe/utils.py
@@ -2,6 +2,6 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 
-def send_code_to_mail(email, code):
+def send_code_mail(email, code):
     link = settings.ALLOWED_HOSTS[0] + ':8000/authe/confirm/'
     return send_mail('Blog', link + code, settings.EMAIL_FROM, [email,])
--- src/authe/views.py
+++ src/authe/views.py
@@ -1,21 +1,36 @@
 from django.shortcuts import render
 from .forms import AuthorForm
 from .models import Author, ConfirmCode
-from .utils import send_code_to_mail
+from .utils import send_code_mail
 from django.conf import settings
 # Create your views here.
 
 def register(request):
-    form = AuthorForm()
-    if request.method == 'POST':
-        form = AuthorForm(request.POST)
-        if form.is_valid():
-            author = Author(username = request.POST['username'], email = request.POST['email'])
+    form=AuthorForm()
+    if request.method=='POST':
+        save_form=AuthorForm(request.POST)
+        if save_form.is_valid():
+            author=Author(username=request.POST['username'],email=request.POST['email'])
             author.set_password(request.POST['password'])
             author.save()
-            code = ConfirmCode.objects.create(author = author)
-            send_code_to_mail(author.email, code.code)
-    return render(request, 'main/register.html', {'form': form})
+            code= ConfirmCode.objects.create(author=author)
+            send_code_mail(author.email,code.code)
+            message = "Все ок"
+            return render(request,'main/reply.html',{"message":message}) 
+        elif Author.objects.filter(username = request.POST['username'], verified=False) or Author.objects.filter(email=request.POST['email'], verified=False):
+            author = None
+            if Author.objects.filter(email = request.POST['email']): 
+                author = Author.objects.get(email = request.POST['email'])
+            elif Author.objects.filter(username = request.POST['username']): 
+                author = Author.objects.get(username = request.POST['username'])
+            elif author.verified == False:
+                code= ConfirmCode.objects.create(author=author)
+                send_code_mail(author.email,code.code)
+            message = 'Такой пользователь уже существует' 
+            return render(request, 'main/reply.html', {'message':message})    
+        message=save_form.errors
+        return render(request,'main/reply.html',{"message":message})    
+    return render(request,'main/register.html',{'form':form})
 
 def confirm_email(request, code):
     code = ConfirmCode.objects.filter(code = code)
